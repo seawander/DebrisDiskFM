a390ec9dd20cc557a3ff4b4cf587049e2b98f7be
Elodie <echoquet@caltech.edu>
2018-07-20 14:53:08 -0700

    test

---
+++ main.py
@@ -0,0 +1,10 @@
+import mcfostParameterTemplate
+
+n_zone = 1
+n_species = [3]
+n_star = 1
+sampleTemplate = mcfostParameterTemplate.generateMcfostTemplate(n_zone = n_zone, n_species = n_species, n_star = n_star)
+
+sampleTemplate['#Density structure']['zone0']['row0']['zone type'] = 3
+
+mcfostParameterTemplate.display_file(sampleTemplate, save_path='/Users/echoquet/Desktop/test.para')
