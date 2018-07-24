f3dc4a081921e249934d67dd6df3a9b5b708a7ad
Bin Ren <renrenbinbin@gmail.com>
2018-07-24 10:54:55 -0700

    Qr available for convolution to simulate instrument response (Gaussian kernel only)

--- diskmodeling_Qr.py
+++ diskmodeling_Qr.py
@@ -26,10 +26,18 @@ def radialStokes(modeldata = None, mcfostGenerated = True, q = None, u = None):
     
     return qr, ur
 
-def diskmodeling_Qr_main(path = './test/'):
+def diskmodeling_Qr_main(path = './test/', fwhm = None):
+    """"""
+    
     mcfost_disk_pol = fits.getdata(path + 'data_1.65/RT.fits.gz')
     Qr, Ur = radialStokes(mcfost_disk_pol)
     del Ur
+    if fwhm is not None:
+        import scipy
+        sigma = fwhm/2.355 # Convert FWHM to sigma for Gaussian/Normal distribution. https://en.wikipedia.org/wiki/Full_width_at_half_maximum
+        Qr_convolved = scipy.ndimage.filters.gaussian_filter(Qr, sigma)
+        Qr = Qr_convolved
+        del Qr_convolved
     return Qr
 
 # test = diskmodeling_Qr_main()
