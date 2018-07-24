86fa3bc0194185b144ffd137087163103be6b9c8
Bin Ren <renrenbinbin@gmail.com>
2018-07-24 14:00:08 -0700

    individual functions for chi2 calculation, and unit conversion

--- debrisdiskfm/lnlike.py
+++ debrisdiskfm/lnlike.py
@@ -6,7 +6,30 @@ from . import dependencies
 import image_registration
 import astropy.units as units
 
-
+def convertMCFOSTdataToJy(data, wavelength, spatialUnit = 'arcsec', spatialResolution = None):
+    """Convert data in MCFOST units into Jansky/pixel or Jansky/arcsec^2:
+    Input:  data: 2D array, MCFOST-generated data.
+            wavelength: float, wavelength of the data to be converted in micron.
+            spatialUnit: string, default = 'arcsec', which will convert it to arcsec^{-2}; 'pixel' will be pixel^{-1}.
+            spatialResolution: float, unit is arcsec/pixel. This will be used to convert the units to arcsec^{-2}.
+    Output: converted data.
+    """
+    data_with_units = data_units*units.W/units.m**2/units.pixel
+    frequency = ((3e8*units.m/units.s)/(wavelength*units.micron)).to(units.hertz)
+    data_with_units_jy = (data_with_units/frequency).to(units.Jansky/units.pixel)   # convert to Jansky/pixel
+    if spatialUnit == 'arcsec':
+        data_with_units_jy /= spatialResolution**2                                  # convert to Jansky/arcsec^2
+    return data_with_units_jy
+    
+def chi2(data, data_unc, model):
+    """Calculate the chi-squared values for given data and model. 
+    Note: if data_unc has values <= 0, they will be ignored and replaced by NaN.
+    Input:  data: 2D array, observed data.
+            data_unc: 2D array, uncertainty/noise map of the observed data.
+    Output: chi2: float, chi-squared value."""
+    data_unc[np.where(data_unc <= 0)] = np.nan
+    chi2 = np.nansum(((data-model)/data_unc)**2)
+    return chi2
 
 def lnlike_hd191089(path_obs = None, path_model = None, psfs = None, psf_cut_hw = 3):
     """Return the log-likelihood for observed data and modelled data.
@@ -28,8 +51,8 @@ def lnlike_hd191089(path_obs = None, path_model = None, psfs = None, psf_cut_hw 
     nicmos_obs_unc = fits.getdata(path_obs + 'NICMOS/calibrated/HD-191089_NICMOS_F110W_Lib-84_KL-19_NoiseMap.fits')
     nicmos_obs_unc[np.where(nicmos_obs_unc <=0)] = np.nan
     
-    gpi_obs = fits.getdata(path_obs + 'GPI/hd191089_gpi_smooth_mJy_arcsec2.fits')*1e3 #Turn it to Jy/arcsec^2
-    gpi_obs_unc = fits.getdata(path_obs + 'GPI/hd191089_gpi_smooth_mJy_arcsec2_noisemap.fits')*1e3 #Turn it to Jy/arcsec^2
+    gpi_obs = fits.getdata(path_obs + 'GPI/hd191089_gpi_smooth_mJy_arcsec2.fits')/1e3 #Turn it to Jy/arcsec^2
+    gpi_obs_unc = fits.getdata(path_obs + 'GPI/hd191089_gpi_smooth_mJy_arcsec2_noisemap.fits')/1e3 #Turn it to Jy/arcsec^2
     gpi_obs_unc[np.where(gpi_obs_unc <=0)] = np.nan
     
     resolution_stis = 0.05078 # arcsec/pixel
@@ -42,8 +65,10 @@ def lnlike_hd191089(path_obs = None, path_model = None, psfs = None, psf_cut_hw 
     if psfs is None:
         psf_stis = fits.getdata(path_obs + 'STIS/calibrated/STIS_6440K_tinyTIM_oddSize.fits')
         psf_nicmos = fits.getdata(path_obs + 'NICMOS/calibrated/')
+        psfs = [psf_stis, psf_nicmos]
+        
         if psf_cut_hw is not None:
-            psfs[0] = dependencies.cutImage(psf_stis, psf_cut_hw) # a 7*7 PSF would need psf_cut_hw = 3 (then 3*2+1 = 7).
+            psfs[0] = dependencies.cutImage(psf_stis, psf_cut_hw)   # a 7*7 PSF would need psf_cut_hw = 3 (then 3*2+1 = 7).
             psfs[1] = dependencies.cutImage(psf_nicmos, psf_cut_hw) # a 7*7 PSF would need psf_cut_hw = 3
             psfs[0] /= np.nansum(psfs[0])
             psfs[1] /= np.nansum(psfs[1])
@@ -53,31 +78,21 @@ def lnlike_hd191089(path_obs = None, path_model = None, psfs = None, psf_cut_hw 
             psfs[0] /= np.nansum(psfs[0])
             psfs[1] /= np.nansum(psfs[1])
         
-    stis_model = fits.getdata(path_model + 'data_0.5852/RT.fits.gz')
-    # convert the MCFOST units to Jy/arcsec^2
+        
+    # convert the MCFOST units to Jy/arcsec^2, and calculate individual chi2
+    stis_model = fits.getdata(path_model + 'data_0.5852/RT.fits.gz')[0, 0, 0]
     stis_convolved = image_registration.fft_tools.convolve_nd.convolvend(stis_model, psfs[0])
-    stis_model_with_units = stis_convolved*units.W/units.m**2/units.pixel
-    frequency_stis = ((3e8*units.m/units.s)/(0.5852*units.micron)).to(units.hertz)
-    stis_model = (stis_model_with_units/frequency_stis).to(units.Jansky/units.pixel) # convert to Jansky/pixel
-    stis_model /= resolution_stis**2 #convert to Jansky/arscec^2
-
+    stis_model = convertMCFOSTdataToJy(stis_convolved, wavelength = 0.5852, spatialResolution = resolution_stis) #convert to Jansky/arscec^2
+    chi2_stis = chi2(nicmos_obs, stis_obs_unc, stis_model) #return chi-squared value for STIS
     
     nicmos_model_forwarded = fm_klip.klip_fm_main(path = path_model, angles= None, psf = psfs[1]) # already convolved
-    nicmos_model_with_units = nicmos_model_forwarded*units.W/units.m**2/units.pixel
-    frequency_nicmos = ((3e8*units.m/units.s)/(1.12347*units.micron)).to(units.hertz)
-    nicmos_model_forwarded = (nicmos_model_with_units/frequency_nicmos).to(units.Jansky/units.pixel) # convert to Jansky/pixel
-    nicmos_model_forwarded /= resolution_nicmos**2 #convert to Jansky/arscec^2
+    nicmos_model = convertMCFOSTdataToJy(nicmos_model_forwarded, wavelength = 1.12347, spatialResolution = resolution_nicmos) #convert to Jansky/arscec^2
+    chi2_nicmos = chi2(nicmos_obs_unc, nicmos_obs_unc, nicmos_model) #return chi-squared value for NICMOS
+    
     
     gpi_model = diskmodeling_Qr.diskmodeling_Qr_main(path = path_model, fwhm = 3.8) 
     # FWHM = 3.8 for GPI, as provided in Tom Esposito's HD35841 paper (Section: MCMC Modeling Procedure)
-    gpi_model_with_units = gpi_model*units.W/units.m**2/units.pixel
-    frequency_gpi = ((3e8*units.m/units.s)/(1.65*units.micron)).to(units.hertz)
-    gpi_model = (gpi_model_with_units/frequency_gpi).to(units.Jansky/units.pixel) # convert to Jansky/pixel
-    gpi_model /= resolution_gpi**2 #convert to Jansky/arscec^2
-    
-    ### Chi-squared values
-    chi2_stis = np.nansum(((stis_obs - stis_model)/stis_obs_unc)**2)
-    chi2_nicmos = np.nansum(((nicmos_obs - nicmos_model_forwarded)/nicmos_obs_unc)**2)
-    chi2_gpi = np.nansum(((gpi_obs - gpi_model)/gpi_obs_unc)**2)
+    gpi_model = convertMCFOSTdataToJy(gpi_model, wavelength = 1.65, spatialResolution = resolution_gpi) #convert to Jansky/arscec^2
+    chi2_gpi = chi2(gpi_obs, gpi_obs_unc, gpi_model)  #return chi-squared value for GPI
     
     return -0.5*(chi2_stis+chi2_nicmos+chi2_gpi)
