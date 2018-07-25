ca8be8f972f89b1effbdef2266b2da73341a152d
Bin Ren <renrenbinbin@gmail.com>
2018-07-25 12:48:48 -0700

    set __init__ up

--- debrisdiskfm/__init__.py
+++ debrisdiskfm/__init__.py
@@ -3,10 +3,12 @@ from . import mcfostParameterTemplate
 from . import fm_klip
 from . import lnlike
 from . import mcfostRun
+from . import lnpost
 from . import dependencies
 
 from .lnlike import lnlike_hd191089
 from .mcfostRun import run_hd191089
+from .lnpost import lnpost_hd191089
 
 from .mcfostParameterTemplate import generateMcfostTemplate, display_file
 #from dependencies import addplanet, rotateImage, rotateCube
