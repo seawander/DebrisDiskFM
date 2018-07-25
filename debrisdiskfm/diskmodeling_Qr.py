cf7b2855a68b938ea6bc6f8737beda5ac0874ced
Bin Ren <renrenbinbin@gmail.com>
2018-07-25 12:49:23 -0700

    Change MCFOST Stokes U sign for IAU convention

--- debrisdiskfm/diskmodeling_Qr.py
+++ debrisdiskfm/diskmodeling_Qr.py
@@ -7,8 +7,8 @@ def radialStokes(modeldata = None, mcfostGenerated = True, q = None, u = None):
     if mcfostGenerated:
         #if modeldata has the structure of MCFOST image output
         q = modeldata[1, 0, 0]
-        u = modeldata[2, 0, 0]
-        
+        u = -modeldata[2, 0, 0] # convert the MCFOST definition of U to IAU definition by flipping the sign
+    
     x_cen = (q.shape[1] - 1)/2.0
     y_cen = (q.shape[0] - 1)/2.0
     
@@ -23,7 +23,10 @@ def radialStokes(modeldata = None, mcfostGenerated = True, q = None, u = None):
     phi = np.arctan((y-y_cen)*1.0/(x-x_cen))
     qr = q * np.cos(2 * phi) + u * np.sin(2 * phi)
     ur = -q * np.sin(2 * phi) + u * np.cos(2 * phi)
-    
+    # fits.writeto('/Users/binren/Desktop/test.fits',  modeldata[0, 0, 0], clobber = True)
+    # fits.writeto('/Users/binren/Desktop/test1.fits',  qr, clobber = True)
+    # fits.writeto('/Users/binren/Desktop/test2.fits',  ur, clobber = True)
+    #
     return qr, ur
 
 def diskmodeling_Qr_main(path = './test/', fwhm = None):
