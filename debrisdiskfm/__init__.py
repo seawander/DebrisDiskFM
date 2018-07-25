2d8e1ddc8c7e964c173d7d223fce66e66dab1da7
Bin Ren <renrenbinbin@gmail.com>
2018-07-24 17:11:14 -0700

    init.py setup

--- debrisdiskfm/__init__.py
+++ debrisdiskfm/__init__.py
@@ -5,8 +5,8 @@ from . import lnlike
 from . import mcmc_wrapper_hd191089
 from . import dependencies
 
-from lnlike import lnlike_hd191089
-from mcmc_wrapper_hd191089 import mcmc_wrapper_hd191089
+from .lnlike import lnlike_hd191089
+from .mcmc_wrapper_hd191089 import mcmc_wrapper_hd191089
 
-from mcfostParameterTemplate import generateMcfostTemplate, display_template
+from .mcfostParameterTemplate import generateMcfostTemplate, display_file
 #from dependencies import addplanet, rotateImage, rotateCube
