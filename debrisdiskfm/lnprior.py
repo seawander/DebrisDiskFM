a6a2b73624ef9276136edff484b9f551a3667b88
Bin Ren <renrenbinbin@gmail.com>
2018-10-31 09:35:15 -0400

    prior for fix q3.5

--- debrisdiskfm/lnprior.py
+++ debrisdiskfm/lnprior.py
@@ -46,7 +46,7 @@ def lnprior_hd191089(var_names = None, var_values = None):
             if not (0 <= theta['fmass_0'] <= 1) or (not (0 <= theta['fmass_1'] <= 1)) or (not (0 <= (theta['fmass_0'] + theta['fmass_1']) <= 1)):
                 return -np.inf
         elif var_name == 'a_min':
-            if not (-3 < theta['a_min'] < 1):
+            if not (-3 < theta['a_min'] < 2):
                 return -np.inf
         elif var_name == 'Q_powerlaw':
             if not (3 < theta['Q_powerlaw'] < 6):
