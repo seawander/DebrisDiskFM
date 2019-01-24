58040fabec49e66621317b5c153b81e455cdbb59
Bin Ren <renrenbinbin@gmail.com>
2019-01-23 19:39:32 -0500

    proper notes for Qphi and Uphi, no code change

--- debrisdiskfm/diskmodeling_Qr.py
+++ debrisdiskfm/diskmodeling_Qr.py
@@ -20,9 +20,10 @@ def radialStokes(modeldata = None, mcfostGenerated = True, q = None, u = None):
     y[:] = y_range
     y = y.T
     
-    phi = np.arctan((y-y_cen)*1.0/(x-x_cen))
-    qr = q * np.cos(2 * phi) + u * np.sin(2 * phi)
-    ur = -q * np.sin(2 * phi) + u * np.cos(2 * phi)
+    phi = np.arctan((y-y_cen)*1.0/(x-x_cen))        # This angle is the counterclockwise angle from x-axis, 
+                                                    # which is 90-deg behind the definition in Monnier et al. (2019) https://arxiv.org/pdf/1901.02467.pdf
+    qr = q * np.cos(2 * phi) + u * np.sin(2 * phi)  # The following two lines are the Q_phi and U_phi after the adjustment of 90 degree.
+    ur = -q * np.sin(2 * phi) + u * np.cos(2 * phi) # They are not changed from the previous version.
     qr[np.where(np.isnan(qr))] = 0
     ur[np.where(np.isnan(ur))] = 0
     return qr, ur
