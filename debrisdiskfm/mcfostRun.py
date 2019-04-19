03fb21c246a82ea0bf18c18b92dd46a89ff74974
Bin Ren <renrenbinbin@gmail.com>
2019-04-19 16:36:33 -0400

    increase number of grains for better SPF

--- debrisdiskfm/mcfostRun.py
+++ debrisdiskfm/mcfostRun.py
@@ -393,6 +393,8 @@ def run_hr4796aH2spf(var_names = None, var_values = None, paraPath = None, calcS
 
     param_hr4796aH2spf['#Grain properties']['row0']['Grain type'] = 'DHS'
     param_hr4796aH2spf['#Grain properties']['row5']['amax'] = 1e3
+    param_hr4796aH2spf['#Grain properties']['row5']['nbr_grains'] = 200
+    
 
     param_hr4796aH2spf['#Grain properties']['row1']['Optical indices file'] = 'MgSiO3.dat'
     param_hr4796aH2spf['#Grain properties']['row2']['Optical indices file'] = 'ac_opct.dat'    #'lgsi_opct.dat'
